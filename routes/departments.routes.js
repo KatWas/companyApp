e586d8ceeec3025fb9b7955f5854ae352504979a
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2023-01-17 19:38:43 +0100

    add changes

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -75,7 +75,8 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if(dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      const leftDep = await Department.find()
+      res.json(leftDep);
     }
     else res.status(404).json({ message: 'Not found...' });
   }
